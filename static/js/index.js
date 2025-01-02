c76eff98aad6940b267e6647b0a1199a811c9b01
Arsnyi <arsenyi.popov@gmail.com>
2025-01-03 00:19:58 +0300

    url base

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 const app = () => {
-    const socket = io('http://localhost:3000');
+    const socket = io('https://chat-nest-pgrsql-render.onrender.com');
     const msgInput = document.querySelector('.message-input');
     const msgList = document.querySelector('.messages-list');
     const sendBtn = document.querySelector('.send-btn');
@@ -9,7 +9,7 @@ const app = () => {
     const getMessages = async () => {
         try {
           const { data } = await axios.get(
-            'http://localhost:3000/api/chat'
+            'https://chat-nest-pgrsql-render.onrender.com/api/chat'
           );
     
           renderMessages(data);
